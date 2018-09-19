837cd38d9176e4ee375a72da72ae0e616ec7087c
hucancode <hucancode@gmail.com>
2018-09-20 00:06:16 +0700

    more random mechanic

---
+++ Assets/RandomActivator.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomActivator : MonoBehaviour {
+
+	public MonoBehaviour component;
+	public float hitRate;
+	// Use this for initialization
+	void Start () {
+		component.enabled = false;
+		float r = Random.Range(0.0f, 100.0f);
+		if(r <= hitRate)
+		{
+			component.enabled = true;
+		}
+	}
+}
---
+++ Assets/RandomSelector.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomSelector : MonoBehaviour {
+
+	public MonoBehaviour[] components;
+	// Use this for initialization
+	void Start () 
+	{
+		foreach(MonoBehaviour item in components)
+		{
+			item.enabled = false;
+		}
+		int i = (int)Random.Range(0.0f, components.Length);
+		if(i == components.Length)
+		{
+			i--;
+		}
+		components[i].enabled = true;
+	}
+}
