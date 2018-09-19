cc627030f889458ba8aefad98ee78ec2917af9b8
hucancode <hucancode@gmail.com>
2018-09-19 23:58:26 +0700

    better cross passer behavior

---
+++ Assets/Script/DelayActivator.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DelayActivator : MonoBehaviour {
+
+	public MonoBehaviour component;
+	public float delayTime;
+	public float accumulatedTime;
+	// Use this for initialization
+	void Start ()
+	{
+		accumulatedTime = 0.0f;
+		component.enabled = false;
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate ()
+	{
+		if(component.enabled)
+		{
+			return;
+		}
+		accumulatedTime += Time.fixedDeltaTime;
+		if(accumulatedTime > delayTime)
+		{
+			component.enabled = true;
+		}
+	}
+}
