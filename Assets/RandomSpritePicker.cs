69a55ad4b98b47ccffc61887e51e8e6fc12f45d6
hucancode <hucancode@gmail.com>
2018-09-22 21:27:58 +0700

    more random mechanic

---
+++ Assets/RandomSpritePicker.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomSpritePicker : MonoBehaviour {
+
+	public Sprite[] spites;
+	public SpriteRenderer spriteRenderer;
+	// Use this for initialization
+	void Start () {
+		int i = (int)Random.Range(0.0f, spites.Length);
+		if(i == spites.Length)
+		{
+			i--;
+		}
+		spriteRenderer.sprite = spites[i];
+	}
+}
