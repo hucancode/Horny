2165e6868845258c1b69c3fb27d3c86bd13f0075
hucancode <hucancode@gmail.com>
2018-10-02 00:18:45 +0700

    add racer generator, fix track positioning

--- Assets/Script/TrackGenerator.cs
+++ Assets/Script/TrackGenerator.cs
@@ -18,7 +18,7 @@ public class TrackGenerator : MonoBehaviour {
 		
 		Vector3 position = Vector3.zero;
 		position.x = transform.parent.position.x;
-		position.y = transform.parent.position.y + trackLength;
+		position.y = transform.parent.position.y;
 		position.z = transform.parent.position.z;
 
 		int i = (int)Random.Range(0.0f, seeds.Length);
@@ -26,6 +26,9 @@ public class TrackGenerator : MonoBehaviour {
 		{
 			i--;
 		}
+		TrackGenerator next_track_info = seeds[i].GetComponentInChildren<TrackGenerator>();
+		float next_track_length = next_track_info.trackLength;
+		position.y +=  trackLength/2.0f + next_track_length/2.0f;
 		GameObject clone = Instantiate(seeds[i], position, Quaternion.identity);
 		// TODO: implement track pool that actually is a pool
 		TrackPool.instance.Push(clone);
