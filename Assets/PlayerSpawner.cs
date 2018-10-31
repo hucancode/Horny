a9ae27ac4fb7baf510d8393e361ec0c56d088ad1
hucancode <hucancode@gmail.com>
2018-10-31 23:56:10 +0700

    Update Assets/PlayerSpawner.cs

--- Assets/PlayerSpawner.cs
+++ Assets/PlayerSpawner.cs
@@ -6,7 +6,7 @@ public class PlayerSpawner : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-        PlayerManager.instance.Spawn();
+		PlayerManager.instance.Spawn();
 	}
 	
 	// Update is called once per frame
