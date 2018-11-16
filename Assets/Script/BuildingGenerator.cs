436322dc9082040656f5901723a7b2fb5aa065ce
hucancode <hucancode@gmail.com>
2018-11-16 22:11:57 +0700

    update racer generator behavior

--- Assets/Script/BuildingGenerator.cs
+++ Assets/Script/BuildingGenerator.cs
@@ -8,21 +8,18 @@ public class BuildingGenerator : MonoBehaviour {
 	public float margin;
 	public float marginVar;
 	public bool flip;
-
-	private const float HOUSE_Z = 0.0f;
-	private const float HOUSE_Z_INC = 1.0f;
 	
 	void OnDrawGizmos()
 	{
 		Gizmos.color = new Color(0, 1, 0, 0.2f);
 		Gizmos.DrawCube(transform.position, new Vector3(spawnArea.x, spawnArea.y, 1));
+		//GameManager.DrawGizmoString(transform.position, "Building Generator");
 	}
 
 	void Start ()
 	{
 		float y = -spawnArea.y*0.5f;
 		float max_y = y + spawnArea.y;
-		float z = 0.0f;
 		while(y < max_y)
 		{
 			int i = (int)Random.Range(0.0f, seeds.Length);
@@ -34,21 +31,28 @@ public class BuildingGenerator : MonoBehaviour {
 			Vector3 position = Vector3.zero;
 			position.x = transform.position.x;
 			position.y = transform.position.y + y;
-			position.z = HOUSE_Z + z;
-			z += HOUSE_Z_INC;
+			position.z = position.y % 100.0f;
+			
+			bool buggy = position.z > 90.0f;
+			// this method of moduling y for z has some downside, we need to hide it
+			// by avoiding buggy case
+			if(buggy)
+			{
+				y += 100 - position.z;
+				continue;
+			}
 
 			GameObject clone = Instantiate(seeds[i], position, Quaternion.identity);
 			BoxCollider2D collider = clone.GetComponent<BoxCollider2D>();
 			SpriteRenderer sprite_renderer = clone.GetComponent<SpriteRenderer>();
 
-			y += collider.size.y * clone.transform.lossyScale.y;
+			y += collider.size.y;
+			y += Random.Range(margin - marginVar, margin + marginVar);
 			if(y >= max_y)
 			{
 				Destroy(clone);
 				break;
 			}
-			y += Random.Range(margin - marginVar, margin + marginVar);
-
 			clone.transform.parent = transform;
 			sprite_renderer.flipX = flip;
 		}
--- Assets/Script/GameManager.cs
+++ Assets/Script/GameManager.cs
@@ -164,4 +164,26 @@ public class GameManager : MonoBehaviour {
 		clone.GetComponent<GameOverPopup>().SetCrashReason(reason);
 		state = State.GameOver;
 	}
+	static public void DrawGizmoString(Vector3 worldPos, string text, Color? colour = null) 
+	{
+		UnityEditor.Handles.BeginGUI();
+
+		var restoreColor = GUI.color;
+
+		if (colour.HasValue) GUI.color = colour.Value;
+		var view = UnityEditor.SceneView.currentDrawingSceneView;
+		Vector3 screenPos = view.camera.WorldToScreenPoint(worldPos);
+
+		if (screenPos.y < 0 || screenPos.y > Screen.height || screenPos.x < 0 || screenPos.x > Screen.width || screenPos.z < 0)
+		{
+			GUI.color = restoreColor;
+			UnityEditor.Handles.EndGUI();
+			return;
+		}
+
+		Vector2 size = GUI.skin.label.CalcSize(new GUIContent(text));
+		GUI.Label(new Rect(screenPos.x - (size.x / 2), -screenPos.y + view.position.height + 4, size.x, size.y), text);
+		GUI.color = restoreColor;
+		UnityEditor.Handles.EndGUI();
+	}
 }
